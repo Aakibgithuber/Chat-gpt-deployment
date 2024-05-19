a2b1275bd62659a1d7d4742e1bafc62fa4bcdd71
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2024-05-19 07:57:47 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "demo-gpt121" # Replace with your actual S3 bucket name
+    bucket = "demo1212221" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
