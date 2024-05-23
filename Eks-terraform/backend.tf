3d38ac7f1096b39faa76c975e5bb54b7337ff74b
dieunor <99774254+dieunor@users.noreply.github.com>
2024-05-23 14:59:53 -0400

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "chatgpts3" # Replace with your actual S3 bucket name
+    bucket = "chatgpt-app-1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
